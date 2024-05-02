afffbf0f55e177d7306a998e167be90d3ed42d26
naveenprasanth <naveenprasanth2@gmail.com>
2024-05-02 12:41:57 +0530

    added few more junit test cases

--- src/main/java/com/basictest/Calculator.java
+++ src/main/java/com/basictest/Calculator.java
@@ -2,4 +2,5 @@ package com.basictest;
 
 public class Calculator {
     public int integerDivision(int dividend, int divisor){return dividend/divisor;}
+    public int integerSubtraction(int minuend, int subtrahend) {return minuend - subtrahend;}
 }
--- src/test/java/com/basictest/CalculatorTest.java
+++ src/test/java/com/basictest/CalculatorTest.java
@@ -12,4 +12,11 @@ class CalculatorTest {
         int result = calculator.integerDivision(4, 2);
         assertEquals(2, result, "This method should return 2 and that should be asserted");
     }
+
+    @Test
+    void integerSubtraction(){
+        Calculator calculator = new Calculator();
+        int result = calculator.integerSubtraction(2, 2);
+        assertEquals(0, result, "It should give you the subtracted value");
+    }
 }
